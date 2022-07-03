6e59af158f5003c2b37bfab2a09c3a2c103ef2d7
Natalia Bubnova <bubnova.natalja89@gmail.com>
2022-07-03 14:04:59 +0300

    fix ошибки

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,10 +3,10 @@ const API_URL = 'https://restcountries.com/v3.1/name';
 export function fetchCountries(name) {
     return fetch(`${API_URL}/${name}?fields=name,capital,population,flags,languages`)
         .then( response => {
-            if (response.ok) {
-                return response.json();
+            if (!response.ok) {
+                throw new Error(response.status);
             }
-        return Notify.failure('Oops, there is no country with that name');
+            return response.json();
         }
     );
 }
